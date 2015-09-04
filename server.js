c41e8c2911e4d88a2b7331eefde59236703755ac
FelipeGangrel <felipefrog@gmail.com>
2015-09-04 13:33:25 -0300

    agora vai?

--- server.js
+++ server.js
@@ -92,7 +92,36 @@ apiRouter.post('/authenticate', function(req, res){
     });
 });
 
+// middleware to use for all requests
+apiRouter.use(function(req, res, next){
+  // do logging
+  console.log('Alguém chegou em nosso APP');
+  // check header or url parameters or POST parameters for token
+  var token = req.body.token || req.param('token') || req.headers['x-access-token'];
+  //decode token
+  if (token) {
+    // verifies secret and expiration
+    jwt.verify(token, superSecret, function(err, decoded){
+      if (err){ return res.status(403).send({
+          success: false,
+          message: 'Falha ao autenticar token.'
+        });
+      }else{
+        // if everything is good, save to request for use in other routes
+        req.decoded = decoded;
+        next();
+      }
+    });
+  }else{
+    // if there is no token
+    // return an HTTP response o 403 (Access Forbidden) and an error message
+    return res.status(403).send({
+      success: false,
+      message: 'Nenhum token foi fornecido.'
+    });
+  }
 
+});
 
 /* test router to make sure everything is working when accessing at GET
 http://localhost:8080/api */
