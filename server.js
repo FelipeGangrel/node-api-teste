45e78a00dde022b0690ddff465b831e9a04ff0c3
FelipeGangrel <felipefrog@gmail.com>
2015-09-04 13:21:52 -0300

    miquinha

--- server.js
+++ server.js
@@ -36,9 +36,8 @@ app.use(morgan('dev'));
 // database hosted on mongolab mongodb://username:password@ds042888...
 //mongoose.connect('mongodb://admin:admin@ds042888.mongolab.com:42888/db_teste');
 
-mongoose.connect('mongodb://admin:hAQlSdF3gKr9@'.$OPENSHIFT_MONGODB_DB_HOST.':'.$OPENSHIFT_MONGODB_DB_PORT.'/app1');
 
-//mongoose.connect('mongodb://admin:GzPBONANYt@node65530-foco001.jelasticlw.com.br/teste');
+mongoose.connect('mongodb://foco:foco@node65626-foco001.jelasticlw.com.br/teste');
 
 // ROUTES FOR OUR API
 // =============================================================================
@@ -93,36 +92,7 @@ apiRouter.post('/authenticate', function(req, res){
     });
 });
 
-// middleware to use for all requests
-apiRouter.use(function(req, res, next){
-  // do logging
-  console.log('Alguém chegou em nosso APP');
-  // check header or url parameters or POST parameters for token
-  var token = req.body.token || req.param('token') || req.headers['x-access-token'];
-  //decode token
-  if (token) {
-    // verifies secret and expiration
-    jwt.verify(token, superSecret, function(err, decoded){
-      if (err){ return res.status(403).send({
-          success: false,
-          message: 'Falha ao autenticar token.'
-        });
-      }else{
-        // if everything is good, save to request for use in other routes
-        req.decoded = decoded;
-        next();
-      }
-    });
-  }else{
-    // if there is no token
-    // return an HTTP response o 403 (Access Forbidden) and an error message
-    return res.status(403).send({
-      success: false,
-      message: 'Nenhum token foi fornecido.'
-    });
-  }
 
-});
 
 /* test router to make sure everything is working when accessing at GET
 http://localhost:8080/api */
